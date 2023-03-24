6b03107e5101f4694b1681b4bcfbc95c666a37ec
Andrewsj3 <andrewsj3@middleton.school.nz>
2023-03-25 08:43:45 +1300

    Added ability to restock comics

--- comic_store.py
+++ comic_store.py
@@ -1,3 +1,6 @@
+"""Restock component: Allow the user to restock copies of a comic
+Jack Andrews
+25/3/23"""
 import customtkinter as ctk
 
 
@@ -7,6 +10,9 @@ class GUI:
         self.root.title("Comic Book Store")
         mainframe = ctk.CTkFrame(self.root, fg_color="transparent")
         mainframe.grid()
+        self.vcmd = mainframe.register(self.validate)
+        # This allows us to validate the user's input when they enter how many
+        # copies they want to restock/sell
         self.header_lbl = ctk.CTkLabel(
             mainframe, text="Comic Book Store", font=(
                 "Calibri", 24, "bold"))
@@ -48,19 +54,33 @@ class GUI:
         ctk.CTkLabel(
             mainframe,
             text="Number of comics:").grid(
-            row=2,
+            row=3,
             column=1,
             sticky='w')
 
+        self.comic_var = ctk.StringVar(mainframe, value="Super Dude")
         self.comic_opt = ctk.CTkOptionMenu(
-            mainframe, values=[
-                "Super Dude", "Lizard Man", "Water Woman"])
+            mainframe,
+            values=[
+                "Super Dude",
+                "Lizard Man",
+                "Water Woman"],
+            variable=self.comic_var)
         self.comic_opt.grid(row=1, column=2, sticky='w', padx=10)
 
-        self.num_comics = ctk.StringVar(mainframe)
+        self.num_comics_var = ctk.StringVar(mainframe)
+        self.bad_text_lbl = ctk.CTkLabel(
+            mainframe, text_color="red", font=(
+                "Calibri", 10), text='')
+        # This label is for when the user inputs an invalid character
+        self.bad_text_lbl.grid(row=2, column=2)
         self.comics_entry = ctk.CTkEntry(
-            mainframe, width=100, textvariable=self.num_comics)
-        self.comics_entry.grid(row=2, column=2, sticky='ew', padx=10)
+            mainframe, width=100, textvariable=self.num_comics_var)
+        self.comics_entry.configure(
+            validate="key", validatecommand=(
+                self.vcmd, "%P"))
+        # Configuring how the validation command is run
+        self.comics_entry.grid(row=3, column=2, sticky='ew', padx=10)
 
         self.sell_btn = ctk.CTkButton(
             mainframe,
@@ -69,8 +89,9 @@ class GUI:
             height=20,
             fg_color="green",
             hover_color="#006400")
+        # No functionality yet
         self.sell_btn.grid(
-            row=3,
+            row=4,
             column=1,
             ipadx=5,
             ipady=10,
@@ -78,9 +99,13 @@ class GUI:
             pady=5)
 
         self.restock_btn = ctk.CTkButton(
-            mainframe, text="Restock Comic(s)", width=0, height=20)
+            mainframe,
+            text="Restock Comic(s)",
+            width=0,
+            height=20,
+            command=self.stock_comics)
         self.restock_btn.grid(
-            row=3,
+            row=4,
             column=2,
             ipadx=5,
             ipady=10,
@@ -88,6 +113,42 @@ class GUI:
             padx=10,
             pady=5)
         self.root.mainloop()
+
+    def stock_comics(self):
+        amnt = self.num_comics_var.get().replace(' ', '')
+        # Allows user to input values like '1 000'
+        if amnt == '':
+            return
+        elif int(amnt) == 0:
+            return
+        # Checking the amount is a valid integer
+        amnt = int(amnt)
+        comic = self.comic_var.get()
+        if comic == "Super Dude":
+            self.dude_stock += amnt
+            self.dude_lbl.configure(
+                text=f"Super Dude: number in stock: {self.dude_stock}")
+        elif comic == "Lizard Man":
+            self.lizard_stock += amnt
+            self.lizard_lbl.configure(
+                text=f"Lizard Man: number in stock: {self.lizard_stock}")
+        elif comic == "Water Woman":
+            self.woman_stock += amnt
+            self.woman_lbl.configure(
+                text=f"Water Woman: number in stock: {self.woman_stock}")
+
+    def validate(self, text):  # Prevents the user from entering a non-integer
+        text = text.replace(" ", '')
+        if text == "":
+            return True
+        try:
+            int(text)
+            self.bad_text_lbl.configure(text="")
+            return True
+        except ValueError:
+            self.bad_text_lbl.configure(text=f"Bad character '{text[-1]}'")
+            # Telling the user exactly what was invalid
+            return False
 
 
 def main():
